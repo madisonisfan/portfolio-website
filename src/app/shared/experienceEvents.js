0aaa6963be0e38e88080a0a1a776aa0e6a9161a3
Madison Isfan <isfanmadison@gmail.com>
2022-08-18 17:33:40 -0700

    First Commit

---
+++ src/app/shared/experienceEvents.js
@@ -0,0 +1,67 @@
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+
+export const EVENTS = [
+  {
+    id: 0,
+    title: "TrainrAI app",
+    dateRange: "Nov 2021 - in progress",
+    icon: "swift",
+  
+    achievements: [],
+    description:
+      "Teamed up with my dad to build an app that helps people create and stay consistent with healthy habits. This is my first SwiftUI project.",
+    languages: ["SwiftUI"],
+    librariesAndFrameworks: [],
+  },
+  {
+    id: 1,
+    title: "Nucamp Fullstack Bootcamp",
+    dateRange: "Feb 2021 - July 2021",
+    icon: "react",
+    achievements: ["Honors for final project"],
+    description:
+      "By the end of this bootcamp, I was able to build dynamic web and mobile app with the use of React and React Native. As part of the bootcamp, I was taught how to create a backend for a web app. On my own time, I taught myself how to build the backend for mobile apps. ",
+    languages: ["JavaScript", "CSS/SCSS", "HTML"],
+    librariesAndFrameworks: [
+      "React",
+      "React Native",
+      "Redux",
+      "Bootstrap",
+      "jQuery",
+    ],
+  },
+
+  {
+    id: 3,
+    title: "Web Programming with Python and Javascript (HarvardX)",
+    dateRange: "Aug 2020 - Jan 2021",
+    icon: "python",
+    description:
+      "This was my first true experience with web development. During the span of the course, I went from reviewing HTML to learning about various was to test and security and scaling.",
+    achievements: [],
+    languages: ["JavaScript", "Python", "SQL", "CSS", "HTML"],
+    librariesAndFrameworks: ["Django"],
+  },
+  {
+    id: 4,
+    title: "Santa Monica College - Computer Science Major",
+    icon: "laptop",
+    description:
+      "During the 2 years of my studies, I joined the STEM program and the computer science club as well.",
+    achievements: ["Dean's Honor List", "2 Scholarships Through STEM program"],
+    dateRange: "Sept 2018 - June 2020",
+    languages: ["C", "C++", "HTML"],
+    librariesAndFrameworks: [],
+  },
+  {
+    id: 5,
+    title: "Graduated HighSchool",
+    icon: "graduationCap",
+    dateRange: "June 2018",
+    achievements: ["4.0 GPA"],
+    description:
+      "Before graduating, I began learning computer science through a couple of high school classes and Codecademy",
+    languages: [],
+    librariesAndFrameworks: [],
+  },
+];
